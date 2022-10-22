d14031decd75024f3f905ed5768c75e90c04ddb6
prisonlifestillcool <86331292+SscripterSs@users.noreply.github.com>
2022-10-22 18:53:37 -0400

    Create xsphub.lua

---
+++ xsphub.lua
@@ -0,0 +1,9 @@
+local OrionLib = loadstring(game:HttpGet(('https://raw.githubusercontent.com/shlexware/Orion/main/source')))()
+
+
+OrionLib:MakeNotification({
+	Name = "Error",
+	Content = "Script Not Out Check Back At: 12/10/22",
+	Image = "rbxassetid://4483345998",
+	Time = 20
+})
